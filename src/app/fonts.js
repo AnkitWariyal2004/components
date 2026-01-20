01b982cbcd12b9857aacb9caf8b053cb057b45c0
Ankit Wariyal <wariyalankit436@gmail.com>
2026-01-20 12:54:26 +0530

    Fix Geist font setup and resolve 404 issue

--- src/app/fonts.js
+++ src/app/fonts.js
@@ -3,12 +3,12 @@ import localFont from "next/font/local";
 export const geistSans = localFont({
   src: [
     {
-      path: "../../public/fonts/Geist-Medium.otf",
+      path: "./fonts/Geist-Medium.otf",
       weight: "500",
       style: "normal",
     },
     {
-      path: "../../public/fonts/Geist-SemiBold.otf",
+      path: "./fonts/Geist-SemiBold.otf",
       weight: "600",
       style: "normal",
     },
